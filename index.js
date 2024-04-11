6ceeb7e785b813a049c190d4cb1eba9654be8b88
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-11 16:19:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ import {
 // Firebase 구성 정보 설정
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBKiu3vfUnvjHx5_M4XD25B6hY26LNPfEw',
-  authDomain: 'sparta-77b4a.firebaseapp.com',
-  projectId: 'sparta-77b4a',
-  storageBucket: 'sparta-77b4a.appspot.com',
-  messagingSenderId: '541201543394',
-  appId: '1:541201543394:web:0913c44614cba2b523eebb',
-  measurementId: 'G-97BRQ5KXHR',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Firebase 인스턴스 초기화
